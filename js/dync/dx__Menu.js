c8a42bbdf9254b39588c140579f2a9dda4abe7bc
caicangcua <36462470+caicangcua@users.noreply.github.com>
2019-04-23 11:30:10 +0700

    Update dx__Menu.js

--- js/dync/dx__Menu.js
+++ js/dync/dx__Menu.js
@@ -87,6 +87,7 @@
                 var val = parseInt($spin.val());
                 $("<p>S3: " + val + "</p>").insertBefore($('.giohang'));
                 var cong = qty.hasClass('qty-plus');
+                $("<p>S4: " + cong + "</p>").insertBefore($('.giohang'));
                 //if (val > 1 || cong) {
                 //    $("<p>S1: " + "</p>").insertBefore($('.giohang'));
                 //    UptCart(cartItem, val + ((cong) ? 1 : -1));
