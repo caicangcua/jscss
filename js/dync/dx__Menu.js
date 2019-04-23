ff9e732639a897097391a272132e099cf970452e
caicangcua <36462470+caicangcua@users.noreply.github.com>
2019-04-23 11:56:20 +0700

    Update dx__Menu.js

--- js/dync/dx__Menu.js
+++ js/dync/dx__Menu.js
@@ -90,8 +90,10 @@
                 $("<p>S4: " + cong + "</p>").insertBefore($('.giohang'));
                 if (val > 1 || cong==true) {
                     $("<p>S1: " + "</p>").insertBefore($('.giohang'));
-                    //UptCart(cartItem, val + ((cong) ? 1 : -1));
-                    //$("<p>S2: " + "</p>").insertBefore($('.giohang'));
+                    var newVal = val + parseInt(((cong) ? 1 : -1));
+                    $("<p>newVal: " + newVal + "</p>").insertBefore($('.giohang'));
+                    UptCart(cartItem, newVal);
+                    $("<p>UptCart: " + "</p>").insertBefore($('.giohang'));
                     //QTY(cartItem.parent());
                     //$("<p>S3: " + "</p>").insertBefore($('.giohang'));
                     //setTimeout(function () { cartItem.find('input').select(); }, 300);
